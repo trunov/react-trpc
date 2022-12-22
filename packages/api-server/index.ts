a5904d88f27ec875beb7e6a8efe823d131f9e291
trunov <k.trunov.k@yandex.ru>
2022-12-22 23:00:50 +0200

    add getMessage and addMessage logic

--- packages/api-server/index.ts
+++ packages/api-server/index.ts
@@ -1,6 +1,13 @@
 import express from "express";
+import cors from "cors";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import { inferAsyncReturnType, initTRPC } from "@trpc/server";
+import { z } from "zod";
+
+interface ChatMessage {
+  user: string;
+  message: string;
+}
 
 const createContext = ({
   req,
@@ -10,15 +17,31 @@ const createContext = ({
 type Context = inferAsyncReturnType<typeof createContext>;
 const t = initTRPC.context<Context>().create();
 
+const messages: ChatMessage[] = [
+  { user: "Kris", message: "Hello" },
+  { user: "Nils", message: "hi" },
+];
+
 const appRouter = t.router({
   hello: t.procedure.query((req) => {
-    return "Hello world";
+    return "Hello world I";
   }),
+  getMessages: t.procedure.input(z.number().default(10)).query(({ input }) => {
+    return messages.slice(-input);
+  }),
+  addMessage: t.procedure
+    .input(z.object({ user: z.string(), message: z.string() }))
+    .mutation(({ input }) => {
+      messages.push(input);
+    }),
 });
+
+export type AppRouter = typeof appRouter;
 
 const app = express();
 const port = 8080;
 
+app.use(cors());
 app.use(
   "/trpc",
   trpcExpress.createExpressMiddleware({
--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,14 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { trpc } from "./trpc";
 
 import "./index.scss";
+import { httpBatchLink } from "@trpc/client";
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: client</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Tailwind</div>
-  </div>
-);
+const AppContent = () => {
+  const [inputValue, setInputValue] = useState({
+    user: "",
+    message: "",
+  });
+
+  const handleChange = (evt: any) => {
+    const value = evt.target.value;
+    setInputValue({
+      ...inputValue,
+      [evt.target.name]: value,
+    });
+  };
+
+  const clearInputState = () => {
+    setInputValue({ user: "", message: "" });
+  };
+
+  const hello = trpc.hello.useQuery();
+  const getMessages = trpc.getMessages.useQuery();
+
+  const addMessage = trpc.addMessage.useMutation();
+  const onAdd = () => {
+    addMessage.mutate(
+      {
+        message: inputValue.message,
+        user: inputValue.user,
+      },
+      {
+        onSuccess: () => {
+          getMessages.refetch();
+        },
+      }
+    );
+    clearInputState();
+  };
+
+  return (
+    <div className="flex flex-col space-y-4 mt-10 text-2xl mx-auto max-w-6xl ">
+      <div>{JSON.stringify(hello.data)}</div>
+
+      {getMessages.data &&
+        getMessages.data.map((row) => (
+          <div
+            className="overflow-hidden leading-normal rounded-lg"
+            role="alert"
+          >
+            <p className="px-4 py-3 font-bold text-purple-100 bg-purple-800">
+              {row.user}
+            </p>
+            <p className="px-4 py-3 text-purple-700 bg-purple-100 ">
+              {row.message}
+            </p>
+          </div>
+        ))}
+      <form onSubmit={() => onAdd} className="flex flex-col space-y-3">
+        <input
+          onChange={handleChange}
+          value={inputValue.user}
+          name="user"
+          placeholder="username"
+          type="text"
+          className="px-4 py-3"
+        />
+        <input
+          onChange={handleChange}
+          value={inputValue.message}
+          name="message"
+          placeholder="message"
+          type="text"
+          className="px-4 py-3"
+        />
+      </form>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        disabled={addMessage.isLoading}
+        onClick={onAdd}
+      >
+        Add message
+      </button>
+      {addMessage.error && (
+        <p>Something went wrong! {addMessage.error.message}</p>
+      )}
+    </div>
+  );
+};
+
+const App = () => {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [httpBatchLink({ url: "http://localhost:8080/trpc" })],
+    })
+  );
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <AppContent />
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
+};
 ReactDOM.render(<App />, document.getElementById("app"));
