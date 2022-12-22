837261a24e2447b2d4fbe0c203e3b436e1db657a
trunov <k.trunov.k@yandex.ru>
2022-12-22 15:16:12 +0200

    add trpc integration to express and Hello World query

--- packages/api-server/index.ts
+++ packages/api-server/index.ts
@@ -1,11 +1,31 @@
 import express from "express";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { inferAsyncReturnType, initTRPC } from "@trpc/server";
+
+const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({});
+
+type Context = inferAsyncReturnType<typeof createContext>;
+const t = initTRPC.context<Context>().create();
+
+const appRouter = t.router({
+  hello: t.procedure.query((req) => {
+    return "Hello world";
+  }),
+});
 
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => {
-  res.send("Hello from api-server");
-});
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
 
 app.listen(port, () => {
   console.log(`api-server listening at http://localhost:${port}`);
